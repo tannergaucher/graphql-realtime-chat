14fe1f16a089777157f88e00abfa76fae072d257
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-05-08 20:46:42 +0800

    scroll latest message into view

--- frontend/src/components/message.js
+++ frontend/src/components/message.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Box, Text, Heading } from 'grommet'
 
 import Avatar from '../components/avatar'
 
-export default function message({ avatarUrl, message, name }) {
+export default function Message({ avatarUrl, message, name, isLatest }) {
+  const messageRef = useRef()
+
+  useEffect(() => {
+    if (isLatest) {
+      messageRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [])
+
   return (
     <Box
-      margin={{ vertical: 'xsmall', horizontal: 'small' }}
-      background="light-1"
+      margin={{ vertical: 'xsmall', horizontal: 'none' }}
       round="small"
       direction="row"
-      pad="xsmall"
       align="center"
+      ref={messageRef}
     >
-      <Box>
-        <Heading
-          level={6}
-          color="neutral-3"
-          margin={{ bottom: 'xsmall', top: 'none' }}
-        >
+      <Avatar avatarUrl={avatarUrl} />
+
+      <Box margin={{ left: 'small' }}>
+        <Heading level={6} color="dark-3" margin="none">
           {name}
         </Heading>
-        <Avatar avatarUrl={avatarUrl} small />
+        <Text>{message}</Text>
       </Box>
-      <Text margin={{ vertical: 'none', left: 'large' }}>{message}</Text>
     </Box>
   )
 }
--- frontend/src/pages/chat-page.js
+++ frontend/src/pages/chat-page.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box } from 'grommet'
 import Message from '../components/message'
 
-class ChatPage extends React.Component {
-  componentWillMount() {
-    this.props.subscribeToNewMessages()
+function ChatPage({ messages, loading, subscribeToNewMessages }) {
+  useEffect(() => {
+    subscribeToNewMessages()
+  }, [])
+
+  function isLatestMessage(messages, index) {
+    return index === messages.length - 1
   }
 
-  render() {
-    const { messages, loading } = this.props
-
-    if (loading) return <p>Loading...</p>
-
-    return (
-      <Box background="" fill basis="3/4" pad="small">
-        <Box overflow="scroll">
-          {messages &&
-            messages.map(message => (
-              <Message
-                key={message.id}
-                message={message.message}
-                avatarUrl={message.user.avatarUrl}
-                name={message.user.name}
-              />
-            ))}
-        </Box>
+  if (loading) return <p>loadingz...</p>
+  return (
+    <Box background="" fill basis="3/4">
+      <Box overflow="scroll">
+        {messages &&
+          messages.map((message, index) => (
+            <Message
+              key={message.id}
+              message={message.message}
+              avatarUrl={message.user.avatarUrl}
+              name={message.user.name}
+              isLatest={isLatestMessage(messages, index)}
+            />
+          ))}
       </Box>
-    )
-  }
+    </Box>
+  )
 }
 
 export default ChatPage
